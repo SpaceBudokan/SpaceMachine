e28f218681b48d883dca4ea7448f4fda2ec54c9f
SpaceBudokan <SpaceBudokan@users.noreply.github.com>
2023-01-12 07:21:47 -0600

    Added push

--- spacemachine.c
+++ spacemachine.c
@@ -12,36 +12,57 @@ INT *returnArray;
 INT returnSize = 25;
 INT programCounter = 0;
 INT stackPointer = 0;
+INT returnPointer = 0;
 
+/*Does nothing and does it well. */
+void noOperation(void)
+{
+    return;
+}
+
+void push(void)
+{
+    programCounter++;
+    stackArray[stackPointer] = programRam[programCounter];
+    stackPointer++;
+    return;
+}
+    
+/*Stops the whole thing and exits back to the operating system. */
+void halt(void)
+{
+    free(stackArray);
+    free(returnArray);
+    exit;
+}
 
 void decode(void)
 {
-  /*using a jumptable because some compilers aren't too eager to optimize switch-case loops to jumptablesI'm using a jumptable here because I wanted to see if I could make it work. A sane person would use a switch case loop and let the compiler decide. */
-  void (*jumptable[1])(void) = {halt};
+    /*using a jumptable because some compilers aren't too eager to optimize switch-case loops to jumptablesI'm using a jumptable here because I wanted to see if I could make it work. A sane person would use a switch case loop and let the compiler decide. */
+    void (*jumptable[3])(void) = {noOperation, push, halt};
   
-  jumptable[programRam[pc]]();
-  return;
+    jumptable[programRam[programCounter]]();
+    return;
 }
 
 int main(int argc, char **argv)
 {
-  int i = 0;
-  stackArray = malloc(sizeof(INT) * stackSize);
-  if(stackArray == NULL){
-    printf("ERROR: Unable to allocate stackArray!\n");
-    return 1;
-  }
+    stackArray = malloc(sizeof(INT) * stackSize);
+    if(stackArray == NULL){
+	printf("ERROR: Unable to allocate stackArray!\n");
+	return 1;
+    }
 
-  returnArray = malloc(sizeof(INT) * returnSize);
-  if(returnArray == NULL){
-    printf("ERROR: Unable to allocate returnArray!\n");
-    return 1;
-  }
+    returnArray = malloc(sizeof(INT) * returnSize);
+    if(returnArray == NULL){
+	printf("ERROR: Unable to allocate returnArray!\n");
+	return 1;
+    }
 
     
-  while(programCounter < programLength){
-    decode();
-    programCounter++;
+    while(programCounter < 100){
+	decode();
+	programCounter++;
     }
   
     free(stackArray);
